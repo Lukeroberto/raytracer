4939fb28dcc861c601a1436584b07bbb2f01a21d
Luke Roberto <lukeroberto213@gmail.com>
2023-11-24 01:09:32 -0500

    added fast rand

--- include/utils.h
+++ include/utils.h
@@ -8,8 +8,22 @@ double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+static unsigned int g_seed = 1;
+
+// Used to seed the generator.           
+//inline void fast_srand(int seed) {
+//    g_seed = seed;
+//}
+
+// Compute a pseudorandom integer.
+// Output value in range [0, 32767]
+static inline int fast_rand(void) {
+    g_seed = (214013*g_seed+2531011);
+    return (g_seed>>16)&0x7FFF;
+}
+
 double random_double() {
-    return (double) rand() / ((double) RAND_MAX + 1.0);
+    return (double) fast_rand() / (32767.0);
 }
 
 double random_double_interval(double min, double max) {
